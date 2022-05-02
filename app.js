a453189d9a88f29812621646c7e0932a524156ce
Liudmyla Yevchuck <lol22lil22@gmail.com>
2022-05-02 20:47:19 +0200

    reading files

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+import fs from "fs";
+
+fs.readFile("./1.txt", "utf8", (err, data) => {
+  if (err) {
+    return;
+  }
+  console.log(data);
+});
+
+fs.readFile("./1.txt", { encoding: "base64" }, (err, data) => {
+  if (err) {
+    return;
+  }
+  console.log(data);
+});
+
+fs.readFile("./1.txt", (err, data) => {
+  if (err) {
+    return;
+  }
+  console.log(data.toString("utf8"));
+});
+/*
 import readline from "readline";
 
 const terminal = readline.createInterface({
@@ -10,7 +33,15 @@ terminal.question("How old are you?\n", (age) => {
   terminal.close();
 });
 
-/*import path from "path";
+
+
+
+
+
+
+
+
+import path from "path";
 import { sayHi } from "./actions.js";
 import { name } from "./name.js";
 import dotenv from "dotenv";
