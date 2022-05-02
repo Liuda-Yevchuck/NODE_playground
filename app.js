8983394aa7114da1caedb79e7463c3fe9809b7a5
Liudmyla Yevchuck <lol22lil22@gmail.com>
2022-05-02 20:37:57 +0200

    work with command line

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-import path from "path";
+import readline from "readline";
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question("How old are you?\n", (age) => {
+  console.log(`${age}, huh, nice`);
+  terminal.close();
+});
+
+/*import path from "path";
 import { sayHi } from "./actions.js";
 import { name } from "./name.js";
 import dotenv from "dotenv";
@@ -12,6 +24,7 @@ console.log(minimist(process.argv.slice(2)));
 sayHi(name);
 
 console.log(path.resolve("./app.js"));
+*/
 
 // import {sayHi} from './actions.js'
 // node app.js --x=2 -y=2 --z 4, --x(true)
