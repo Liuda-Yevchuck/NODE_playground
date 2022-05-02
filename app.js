108cba8982b0f649e1666255ccbfe482410b9317
Liudmyla Yevchuck <lol22lil22@gmail.com>
2022-05-02 08:29:31 +0200

    lint staged

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import path from "path";
 import { sayHi } from "./actions.js";
 import { name } from "./name.js";
+import dotenv from "dotenv";
+import minimist from "minimist";
 
-console.log("Wassup :)");
+dotenv.config();
+
+console.log("Wassup :)", process.env.NAME);
+console.log(minimist(process.argv.slice(2)));
+
 sayHi(name);
 
 console.log(path.resolve("./app.js"));
-console.log("abeba asdkljaslkd mama");
 
-//import {sayHi} from './actions.js'
+// import {sayHi} from './actions.js'
+// node app.js --x=2 -y=2 --z 4, --x(true)
